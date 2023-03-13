952d2e861857e7fcbd34dc96315ed0a206c770ac
Anton <pm13gai@gmail.com>
2023-03-13 23:35:03 +0300

    step-1 (context)

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import BurgerConstructor from './order/burgerConstructor'
 import styles from './App.module.scss';
 
 import { getIngredients } from '../utils/burgerApi'
+import { IngredientsListContext } from '../utils/appContext'
 
 
 function App() {
@@ -14,40 +15,26 @@ function App() {
   const [ingredientsList, setIngredientsList] = useState([]);
 
   useEffect(() => {
-
-    getIngredients().then(data => {setData(data.data)}).catch(error => {setErr(error)})
-
+    getIngredients().then(data => { setData(data.data) }).catch(error => { setErr(error) })
   }, [])
 
 
-  const onIngredientClick = (ingredient) => {
-    let count = ingredientsList.reduce((count, el) => {
-      if (el._id === ingredient._id) return count + 1;
-      return count
-    }, 0);
-
-    let ingredientToAdd= { ...ingredient, idForList: ingredient._id + count }
-
-    setIngredientsList([...ingredientsList, ingredientToAdd]);
-
-  }
-  const onChangeIngredientsList = (id) => {
-    setIngredientsList(ingredientsList.slice(0).filter((el) => el.idForList !== id))
-  }
   return (
     <div className="flex flex-column h100pcnt overflow-h">
-      <AppHeader />
-      <div className="flex j-center h100pcnt overflow-h">
-        <div className="flex flex-column h100pcnt overflow-h mr-10">
-          <h1 className={`${styles.title} mt-10 mb-5`}>Соберите бургер</h1>
-          {data && <BurgerIngredients ingredients={data} onIngredientClick={onIngredientClick} ingredientsList={ingredientsList} />}
-          {err && <div>Нет данных</div>}
-        </div>
-        <div className="h100pcnt">
-        {data && <BurgerConstructor ingredientsList={ingredientsList} onChangeIngredientsList={onChangeIngredientsList} bunDetails={data.find(el => el.name === 'Краторная булка N-200i')} />}
-        </div>
+      <IngredientsListContext.Provider value={{ ingredientsList, setIngredientsList }}>
+        <AppHeader />
+        <div className="flex j-center h100pcnt overflow-h">
+          <div className="flex flex-column h100pcnt overflow-h mr-10">
+            <h1 className={`${styles.title} mt-10 mb-5`}>Соберите бургер</h1>
+            {data && <BurgerIngredients ingredients={data} />}
+            {err && <div>Нет данных</div>}
+          </div>
+          <div className="h100pcnt">
+            {data && <BurgerConstructor bunDetails={data.find(el => el.name === 'Краторная булка N-200i')} />}
+          </div>
 
-      </div>
+        </div>
+      </IngredientsListContext.Provider>
     </div>
   );
 }
---
+++ src/utils/appContext.js
@@ -0,0 +1,2 @@
+import React from 'react';
+export const IngredientsListContext = React.createContext();
